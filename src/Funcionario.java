81bcbfdb473bfe81b9c455cb304b7cc8842934fd
senarobin <140357675+senarobin@users.noreply.github.com>
2024-11-05 13:33:41 -0300

    Update Funcionario.java

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -8,15 +8,13 @@ public class Funcionario extends Pessoas implements Gerenciavel {
     private TurnoTrabalho turno;
     private List<RegistroTrabalho> registroTrabalho;
 
-    public Funcionario(String nome, String cpf, Cargo cargo, double salarioBase, TurnoTrabalho turno) {
-        super(nome, cpf);
+    public Funcionario(String nome, String cpf, String endereco, String telefone, Cargo cargo, double salarioBase, TurnoTrabalho turno) {
+        super(nome, cpf, endereco, telefone);
         this.cargo = cargo;
         this.salarioBase = salarioBase;
         this.turno = turno;
         this.registroTrabalho = new ArrayList<>();
     }
-
-
 
     public void registrarHorasTrabalhadas(LocalDateTime entrada, LocalDateTime saida) {
         registroTrabalho.add(new RegistroTrabalho(entrada, saida));
@@ -29,39 +27,30 @@ public class Funcionario extends Pessoas implements Gerenciavel {
     public double calcularSalario(int mes, int ano) {
         double salarioCalculado = salarioBase;
         for (RegistroTrabalho registro : registroTrabalho) {
-            salarioCalculado += registro.getDuracaoTrabalho().toHours() * 10;
+            salarioCalculado += registro.getDuracaoTrabalho().toHours() * 10;  // Ajuste aqui de acordo com a política de pagamento de horas extras
         }
         return salarioCalculado;
     }
 
-    // Métodos de cadastro, atualização e exclusão (não implementados aqui)
     @Override
     public void cadastrar() throws HotelException {
-        // Lógica para cadastrar funcionário
+
     }
 
     @Override
     public void atualizar() throws HotelException {
-        // Lógica para atualizar dados do funcionário
     }
 
     @Override
     public void excluir() throws HotelException {
-        // Lógica para excluir o funcionário
     }
 
-    // Getters e Setters para cargo e nome
     public Cargo getCargo() {
         return this.cargo;
     }
 
     public void setCargo(Cargo novoCargo) {
         this.cargo = novoCargo;
-    }
-
-    @Override
-    public void setNome(String novoNome) {
-        super.setNome(novoNome);
     }
 
     public double getSalarioBase() {
@@ -78,5 +67,18 @@ public class Funcionario extends Pessoas implements Gerenciavel {
 
     public void setTurno(TurnoTrabalho turno) {
         this.turno = turno;
+    }
+
+    public List<RegistroTrabalho> getRegistroTrabalho() {
+        return registroTrabalho;
+    }
+
+    public void setRegistroTrabalho(List<RegistroTrabalho> registroTrabalho) {
+        this.registroTrabalho = registroTrabalho;
+    }
+
+    @Override
+    public void setNome(String novoNome) {
+        super.setNome(novoNome);
     }
 }
